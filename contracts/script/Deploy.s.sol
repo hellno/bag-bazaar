0deb4e610f3de3748a2493411a64602d5877a207
botnet <92654767+0x330a@users.noreply.github.com>
2024-11-17 02:50:41 +1100

    feat: add percentage fee to deployer for initial token supply

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -1,24 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.25 <0.9.0;
 
-import { SocialDexDeployer } from "../src/BagBazaarFactory.sol";
+import "../src/BagBazaarFactory.sol";
 
 import { BaseScript } from "./Base.s.sol";
-import {LpLocker} from "../src/LpLocker.sol";
+import { SocialDexDeployer } from "../src/BagBazaarFactory.sol";
 import {LockerFactory} from "../src/LockerFactory.sol";
+import {LpLocker} from "../src/LpLocker.sol";
 import {console2} from "forge-std/src/console2.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract Deploy is BaseScript {
     function run() public broadcast returns (SocialDexDeployer factory) {
-
-//        address taxCollector_,
-//        address weth_,
-//        address locker_,
-//        address uniswapV3Factory_,
-//        address positionManager_,
-//    uint64 defaultLockingPeriod_,
-//    address swapRouter_
 
         address guy = msg.sender;
 
@@ -26,7 +19,7 @@ contract Deploy is BaseScript {
         address weth = 0x4200000000000000000000000000000000000006;
         address uniFactory = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
         address positionManager = 0xB7F724d6dDDFd008eFf5cc2834edDE5F9eF0d075;
-        uint64 defaultLockingPeriod = 0;
+        uint64 defaultLockingPeriod = 4132317178;
         address swapRouter = 0xd1AAE39293221B77B0C71fBD6dCb7Ea29Bb5B166;
 
 
@@ -39,5 +32,19 @@ contract Deploy is BaseScript {
             defaultLockingPeriod,
             swapRouter
         );
+
+        (Token token, ) = factory.deployToken(
+            "TestToken",
+            "TEST",
+            1_000 * 10 ** 18,
+            -207400,
+            10000,
+            bytes32(uint256(0xe)),
+            guy
+        );
+
+        console2.logAddress(address(token));
+        console2.logUint(token.balanceOf(guy));
+
     }
 }
